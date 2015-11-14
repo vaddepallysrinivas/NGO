5fbb977b192c9dec09737defe998dd94f815c179
Srini@126 <svr.srs@gmail.com>
2015-11-14 13:09:30 +0530

    Techer registation forms

---
+++ NGO/app/index.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+angular.module('ngo').controller('index', index);
+index.$inject = ['$state', '$scope'];
+
+function index($state, $scope) {
+
+
+    var vm = {
+        model: {},
+        init: init
+
+    };
+
+
+    return vm;
+    function init() {
+        
+    }
+
+
+
+}
+
+
+
+
+
